c91e9bf23a95b17a1ebcef98dcb9412edd5d9528
rahmansaleh7 <rahman.saleh7@gmail.com>
2020-02-28 14:34:13 +0700

    Versi 2

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Wire Transfer E-Commerce",
+    'description': "Pembayaran transfer pada e-commerce otomatis rekonsiliasi",
+    'category': 'account',
+    'version': '2.0',
+    'depends': ['website_sale', 'delivery','sale','portal'],
+    'data': [
+        'data/ir_cron_data.xml',
+        'views/account_bank_statement_view.xml',
+        'views/sale_view.xml',
+        'views/company_view.xml',
+        'views/website_payment_transfer_templates.xml',
+        'views/account_payment_view.xml',
+        'views/stock_picking_view.xml',
+        'views/confirm_order.xml',
+    ],
+}
